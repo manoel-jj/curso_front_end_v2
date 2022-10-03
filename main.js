faad1f1fbb4bdc3b8a981fefcd0e067f1dc0ea80
manoel jailton <manoeljailton79@gmail.com>
2022-10-03 10:04:32 -0300

    tentativa-2

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ $(document).ready(function () {
         const listaDeTarefas = $('#lista-tarefas').val();
         const novaTarefa = $('<li style="display:none"></li>');
         $(`<li>${listaDeTarefas}</li>`).appendTo(novaTarefa);
+        $(`<li>${novaTarefa}</li>`).click(function () {
+            $(this).toggleClass("sublinhar")
+        })
 
         $(novaTarefa).appendTo('ul');
         $(novaTarefa).fadeIn(1000);
-        $('#lista-tarefas').val('')
-
     })
 
 
+
 })
+
+
+
+
+
+
+
