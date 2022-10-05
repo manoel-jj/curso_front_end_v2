eedfeeb896134862f1941485d4567850c6362568
manoel jailton <manoeljailton79@gmail.com>
2022-10-05 20:30:04 -0300

    concluido

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ $(document).ready(function () {
         e.preventDefault();
         const listaDeTarefas = $('#lista-tarefas').val();
         const novaTarefa = $('<li style="display:none"></li>');
+        const novaTarefaClick = $(`<li>${listaDeTarefas}</li>`).click(function () {
+            $(this).toggleClass("sublinhar")
+        })
+        $(novaTarefaClick).appendTo('#listas');
+        $('#lista-tarefas').val('');
         $(`<li>${listaDeTarefas}</li>`).appendTo(novaTarefa)
-        $(novaTarefa).appendTo('ul');
         $(novaTarefa).appendTo('novaTarefa');
         $(novaTarefa).fadeIn(1000);
     })
-    $('ul').click(function () {
-        $(this).toggleClass("sublinhar");
 
-    })
 
 })
+
+
 
 
 
