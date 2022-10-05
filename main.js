cfa8a2f20d8ca883e8bcc4148c4a455f556cfce6
manoel jailton <manoeljailton79@gmail.com>
2022-10-04 21:21:38 -0300

    tent.3

--- main.js
+++ main.js
@@ -3,18 +3,26 @@ $(document).ready(function () {
         e.preventDefault();
         const listaDeTarefas = $('#lista-tarefas').val();
         const novaTarefa = $('<li style="display:none"></li>');
-        $(`<li>${listaDeTarefas}</li>`).appendTo(novaTarefa);
-        $(`<li>${novaTarefa}</li>`).click(function () {
-            $(this).toggleClass("sublinhar")
-        })
-
+        $(`<li>${listaDeTarefas}</li>`).appendTo(novaTarefa)
         $(novaTarefa).appendTo('ul');
+        $(novaTarefa).appendTo('novaTarefa');
         $(novaTarefa).fadeIn(1000);
     })
+    $('ul').click(function () {
+        $(this).toggleClass("sublinhar");
 
-
+    })
 
 })
+
+
+
+
+
+
+
+
+
 
 
 
