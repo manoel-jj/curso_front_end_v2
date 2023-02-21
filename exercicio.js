4cdd3b9a38be48e814273b691c62e1d032be2fef
manoel jailton <manoeljailton79@gmail.com>
2023-02-21 11:15:50 -0300

    subindo exercicio ts

---
+++ exercicio.js
@@ -0,0 +1,17 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return `Olá + ${nome}`;
+}
+// Função de multiplicação
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
---
+++ exercicio.ts
@@ -0,0 +1,25 @@
+// Função de multiplicação
+
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2
+
+}
+
+
+  // Função de saudação
+
+function saudacao(nome: string): string {
+    return `Olá + ${nome}`
+
+}  
+
+// Função de multiplicação
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+  }
+  
+  // Função de saudação
+  function saudar(nome: string): string {
+    return `Olá ${nome}`;
+  }
+  
